5eb69323e8c2a6e23ba173b0c2690823d6795d7a
lyssdod <lyssdod@gmail.com>
2016-06-09 01:53:50 +0300

    Added basic media support via subclassing

--- multiupload/fields.py
+++ multiupload/fields.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 from django import forms
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError, FieldError
 from django.utils.translation import ugettext_lazy as _
 
+# feel free to extend this, see
+# http://www.iana.org/assignments/media-types/media-types.xhtml
+MEDIA_TYPES = ['image', 'audio', 'video']
 
-class MultiFileInput(forms.FileInput):
+class MultiUploadMetaInput(forms.ClearableFileInput):
     def render(self, name, value, attrs=None):
         attrs['multiple'] = 'multiple'
-        return super(MultiFileInput, self).render(name, value, attrs)
+        return super(MultiUploadMetaInput, self).render(name, value, attrs)
 
     def value_from_datadict(self, data, files, name):
         if hasattr(files, 'getlist'):
@@ -19,9 +22,7 @@ class MultiFileInput(forms.FileInput):
             else:
                 return [value]
 
-
-class MultiFileField(forms.FileField):
-    widget = MultiFileInput
+class MultiUploadMetaField(forms.FileField):
     default_error_messages = {
         'min_num': _(u'Ensure at least %(min_num)s files are uploaded (received %(num_files)s).'),
         'max_num': _(u'Ensure at most %(max_num)s files are uploaded (received %(num_files)s).'),
@@ -32,19 +33,20 @@ class MultiFileField(forms.FileField):
         self.min_num = kwargs.pop('min_num', 0)
         self.max_num = kwargs.pop('max_num', None)
         self.maximum_file_size = kwargs.pop('max_file_size', None)
-        super(MultiFileField, self).__init__(*args, **kwargs)
+        self.widget = MultiUploadMetaInput(attrs = kwargs.pop('attrs', {}))
+        super(MultiUploadMetaField, self).__init__(*args, **kwargs)
 
     def to_python(self, data):
         ret = []
         data = data or []
         for item in data:
-            i = super(MultiFileField, self).to_python(item)
+            i = super(MultiUploadMetaField, self).to_python(item)
             if i:
                 ret.append(i)
         return ret
 
     def validate(self, data):
-        super(MultiFileField, self).validate(data)
+        super(MultiUploadMetaField, self).validate(data)
         num_files = len(data)
         if len(data) and not data[0]:
             num_files = 0
@@ -57,3 +59,26 @@ class MultiFileField(forms.FileField):
         for uploaded_file in data:
             if self.maximum_file_size and uploaded_file.size > self.maximum_file_size:
                 raise ValidationError(self.error_messages['file_size'] % {'uploaded_file_name': uploaded_file.name})
+
+class MultiMediaInput(MultiUploadMetaInput):
+    pass
+
+class MultiFileInput(MultiUploadMetaField):
+    pass
+
+class MultiMediaField(MultiUploadMetaField):
+    error_messages = {
+        'wrong_type': _(u'Invalid media_type. Valid types are: %(valid_types)s')
+    }
+
+    def __init__(self, *args, **kwargs):
+        self.media_type = kwargs.pop('media_type', 'image')
+
+        if self.media_type not in MEDIA_TYPES:
+            raise FieldError(self.error_messages['wrong_type'] % {'valid_types': ', '.join(MEDIA_TYPES)})
+
+        kwargs.update({ 'attrs': {'accept': '{0}/*'.format(self.media_type)} })
+        super(MultiMediaField, self).__init__(*args, **kwargs)
+
+class MultiFileField(MultiUploadMetaField):
+    pass

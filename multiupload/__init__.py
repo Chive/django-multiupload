e6bf1e5b53ab9b9aa71553abd4b0f26f485a30aa
sobolevn <mail@sobolevn.me>
2016-06-20 10:34:14 +0300

    This commit was primary focused on stylefixes.
    Stylefixes:
    1. added `moduledocs` and class `doc-strings`
    2. stylefixes
    3. integrated `pylint` into the `tox`-testing process
    
    But there are a lot of other changes:
    1. added new classificators to the `setup.py`
    2. added `idea/` to `.gitignore`
    3. added myself to `AUTHORS`
    4. changed the way `LONG_DESCRIPTION` in `setup.py` was created (file was not closed)
    5. forced `# -*- coding: utf-8 -*-` in all files
    6. added `lint` to Travis builds
    7. removed disscused issues

--- multiupload/__init__.py
+++ multiupload/__init__.py
@@ -1,2 +1,8 @@
 # -*- coding: utf-8 -*-
+
+"""
+Dead simple drop-in multi file upload field for Django forms
+using HTML5's multiple attribute. http://chive.ch/django-multiupload
+"""
+
 __version__ = '0.5.1'
--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 # -*- coding: utf-8 -*-
+
+"""
+`setup.py` for `django-multiupload`.
+
+For project information check out:
+https://github.com/Chive/django-multiupload
+
+For `setup.py` information check out:
+https://docs.python.org/2/distutils/setupscript.html
+"""
+
 import os
 
 from setuptools import setup, find_packages
 
 from multiupload import __version__
 
-
 REQUIREMENTS = [
-    'django'
+    'django',
 ]
 
 CLASSIFIERS = [
@@ -18,17 +28,24 @@ CLASSIFIERS = [
     'License :: OSI Approved :: BSD License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
+    'Topic :: Internet :: WWW/HTTP',
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     'Topic :: Software Development',
     'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Topic :: Software Development :: Libraries :: Python Modules',
 ]
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+    LONG_DESCRIPTION = f.read()
 
 setup(
     name='django-multiupload',
     version=__version__,
-    description='Dead simple drop-in multi file upload field for django forms using HTML5\'s multiple attribute.',
-    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    description='Dead simple drop-in multi file upload field '
+                'for django forms using HTML5\'s multiple attribute.',
+    long_description=LONG_DESCRIPTION,
     author='Chive',
     author_email='kim@smuzey.ch',
     url='https://github.com/Chive/django-multiupload',
@@ -38,5 +55,5 @@ setup(
     install_requires=REQUIREMENTS,
     classifiers=CLASSIFIERS,
     include_package_data=True,
-    zip_safe=False
+    zip_safe=False,
 )

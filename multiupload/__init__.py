a89f666024549fc3c3a1477e91ac102e0ffb4425
Kim Thoenen <kim@smuzey.ch>
2021-10-06 23:13:14 +0200

    release 0.6.0

--- multiupload/__init__.py
+++ multiupload/__init__.py
@@ -5,4 +5,4 @@ Dead simple drop-in multi file upload field for Django forms
 using HTML5's multiple attribute. http://chive.ch/django-multiupload
 """
 
-__version__ = '0.5.2'
+__version__ = '0.6.0'
